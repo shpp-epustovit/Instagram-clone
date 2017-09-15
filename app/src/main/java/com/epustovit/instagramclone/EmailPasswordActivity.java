f3b871ee7be40e174dca354dc2feb509d121b637
shpp-epustovit <smithpev@gmail.com>
2017-09-15 20:21:36 +0300

    add signUp and logIn

--- app/src/main/java/com/epustovit/instagramclone/EmailPasswordActivity.java
+++ app/src/main/java/com/epustovit/instagramclone/EmailPasswordActivity.java
@@ -3,8 +3,11 @@ package com.epustovit.instagramclone;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -18,8 +21,63 @@ public class EmailPasswordActivity extends AppCompatActivity implements View.OnC
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private EditText ETemail;
-    private EditText ETpassword;
+
+    Boolean signUpModeActive = true;
+
+    private TextView changeSignupModeTextView;
+    private EditText userEmailEditText;
+    private EditText passwordEditText;
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.changeSignupModeTextView) {
+
+            Button signupButton = (Button) findViewById(R.id.signupButton);
+
+            if (signUpModeActive) {
+
+                signUpModeActive = false;
+                signupButton.setText("Login");
+                changeSignupModeTextView.setText("Or, Signup");
+
+            } else {
+
+                signUpModeActive = true;
+                signupButton.setText("Signup");
+                changeSignupModeTextView.setText("Or, Login");
+
+            }
+
+        }
+
+    }
+
+    public void signUp(View view) {
+
+        EditText userEmailEditText = (EditText) findViewById(R.id.userEmailEditText);
+
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        if (userEmailEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
+
+            Toast.makeText(this, "A username and password are required.", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            if (signUpModeActive) {
+
+                registration(userEmailEditText.getText().toString(),passwordEditText.getText().toString());
+
+            } else {
+
+                signIn(userEmailEditText.getText().toString(),passwordEditText.getText().toString());
+
+            }
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,26 +101,17 @@ public class EmailPasswordActivity extends AppCompatActivity implements View.OnC
             }
         };
 
-        ETemail = (EditText) findViewById(R.id.et_email);
-        ETpassword = (EditText) findViewById(R.id.et_password);
+        changeSignupModeTextView = (TextView) findViewById(R.id.changeSignupModeTextView);
 
-        findViewById(R.id.btn_sign_in).setOnClickListener(this);
-        findViewById(R.id.btn_registration).setOnClickListener(this);
-    }
+        changeSignupModeTextView.setOnClickListener(this);
 
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.btn_sign_in)
-        {
-            signin(ETemail.getText().toString(),ETpassword.getText().toString());
-        }else if (view.getId() == R.id.btn_registration)
-        {
-            registration(ETemail.getText().toString(),ETpassword.getText().toString());
-        }
+
+
 
     }
 
-    public void signin(String email , String password)
+
+    public void signIn(String email , String password)
     {
         mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
